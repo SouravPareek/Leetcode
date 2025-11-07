a1c8eb5f50fe47ee003e172b8ea8f24f078dfd8b
SouravPareek <148560393+SouravPareek@users.noreply.github.com>
2025-11-07 10:57:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (64.39%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -1,4 +1,4 @@
-//recursive approach lec
+ //iterative approach lec
 
 /**
  * Definition for a binary tree node.
@@ -13,31 +13,38 @@
  */
 
 class Solution {
-private:
-    void recursiveInorder(TreeNode* node, vector<int>& ans){
-        if(node == NULL)
-            return;
-        
-        //left side traversal
-        recursiveInorder(node->left, ans);
-
-        //adding the data of node to list
-        ans.push_back(node->val);
-        
-        //traversing towards right
-        recursiveInorder(node->right, ans);
-    }
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
 
-        //new pointer to root node so that root node doesn't get manipulated during process 
-        TreeNode* node = root;
-        recursiveInorder(node, ans);
+        //stack stores past nodes for future accessing
+        stack<TreeNode*> st;
 
+        TreeNode* node = root;
+
+        while(true){
+            //check if at leaf node or not, if not then push onto stack and move left
+            if(node != NULL){
+                st.push(node);
+                node = node->left;
+            }else{
+                //if stack is empty , it implies all the nodes have been traversed thus end the while loop
+                if(st.empty())
+                    break;
+                
+                //if any node remains after traversing below it then move back to the parent node and traverse towards right
+                node = st.top();
+                st.pop();
+                    
+                ans.push_back(node->val);
+
+                //traversing to right half og each subtree
+                node = node->right;
+            }
+        }
         return ans;
     }
 };
 
-//T.C : O(n), as all the nodes will be traversed at least once for inorder traversal
-//S.C : O(h), h->height of the BT for recursion stack and O(n) for answerr array
+//T.C : O(n), as all nodes are traversed at least once
+//S.C : O(h), h is height of the tree as stack will store at most h elements at any moment coz on traversing an element it will pop it out and then only mov to other half of the tree
