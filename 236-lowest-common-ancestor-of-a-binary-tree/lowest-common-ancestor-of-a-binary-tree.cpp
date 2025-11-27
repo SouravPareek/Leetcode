f18f28af74e4d3e388e9894772db96a7707c2d5d
SouravPareek <148560393+SouravPareek@users.noreply.github.com>
2025-11-27 17:42:53 +0530

    Time: 12 ms (64.22%) | Memory: 17.5 MB (39.90%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,36 @@
+//lec approach using dfs
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        //if root is NULL or root = p or root = q then return root as root will be LCA
+        if(root == NULL || root == p || root == q)
+            return root;
+        
+        //traverse towards left and right to find LCA
+        TreeNode* left = lowestCommonAncestor(root->right, p, q);
+        TreeNode* right = lowestCommonAncestor(root->left, p, q);
+        
+        //if left is NULL then return right
+        if(left == NULL)
+            return right;
+        //if right is NULL then return NULL
+        else if(right == NULL)
+            return left;
+        //if both subtrees are not nullptr then thay have wanted pointers: one has p and anther has q, because we can get something differen than nullptr only if we find p,q or LCA, we find LCA in one of subtrees then we will have nullptr in the other one, because both p and q are available in that subtree where LCA is, so the another one is nullptr
+        else
+            return root;
+    }
+};
+
+// T.C : O(n), as all nodes are traversed exatly once
+//S.C : O(h), h->height of the subtree, that is recursion stack, in worst case h = n
