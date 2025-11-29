b85b41eb0bd3f660a96a87c4b39ada6c1bd1d0a3
SouravPareek <148560393+SouravPareek@users.noreply.github.com>
2025-11-29 20:38:52 +0530

    Time: 46 ms (85.67%) | Memory: 56.7 MB (36.59%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var num = init;
+    return{
+        increment : ()=>{
+            return ++num;
+        },
+        decrement : ()=>{
+            return --num;
+        },
+        reset : ()=>{
+            num = init;
+            return num;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
