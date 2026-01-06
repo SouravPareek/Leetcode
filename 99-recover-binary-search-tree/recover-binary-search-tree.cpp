1d4650a9e47f540b5031600c27bb659bb9e77613
Sourav Pareek <148560393+SouravPareek@users.noreply.github.com>
2026-01-06 19:05:22 +0530

    Time: 0 ms (100.00%) | Memory: 60.9 MB (52.54%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -0,0 +1,54 @@
+//optimal approach
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode *first, *prev, *middle, *last;
+
+    //find the current inorder of the give tree
+    void Inorder(TreeNode* node){
+        if(node){
+            Inorder(node->left);
+            
+            //if prev node is not null, that means we are not at first node and than checkif prev node's value is greater than current node's val or not, if yes than it can be the swapped node and similarly check for each node and based on that find the nodes that are swapped
+            if(prev && prev->val > node->val){
+                if(!first){
+                    first = prev;
+                    middle = node;
+                }else{
+                    last = node;
+                }
+            }
+            prev = node;
+
+            Inorder(node->right);
+        }
+    }
+public:
+    void recoverTree(TreeNode* root) {
+        //initialize first, last, middle, prev pointer to null
+        first = last = middle = prev = nullptr;
+
+        //call the inorder
+        Inorder(root);
+
+        //check the nodes and swap the values
+        if(first && last){
+            swap(first->val, last->val);
+        }else if(first && middle){
+            swap(first->val, middle->val);
+        }
+    }
+};
+
+// T.C : O(nlogn), O(n) for collecting current inorder, O(n) fro restoring, O(nlogn) for sorting
+// S.C : O(n), O(n) for storing inorder, O(n) stack space due to recursion
