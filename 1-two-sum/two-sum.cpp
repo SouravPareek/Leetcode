69900526e89d2b775785fc3ef27dfb8cedcb19c6
Sourav Pareek <148560393+SouravPareek@users.noreply.github.com>
2026-02-09 12:35:57 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (18.81%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,13 +5,11 @@ public:
         unordered_map<int, int> hash;
 
         for(int i = 0; i < n; i++){
-            hash[nums[i]] = i;
-        }
-
-        for(int i = 0; i < n; i++){
             int comp = target - nums[i];
             if(hash.find(comp) != hash.end() && hash[comp] != i)
-                return {i, hash[comp]};
+                return {hash[comp], i};
+            
+            hash[nums[i]] = i;
         }
         return {};
     }
