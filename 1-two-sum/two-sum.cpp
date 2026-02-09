be5ebde78af3416bdf2305645ce8078142c5d1e6
Sourav Pareek <148560393+SouravPareek@users.noreply.github.com>
2026-02-09 12:35:08 +0530

    Time: 7 ms (42.92%) | Memory: 15.9 MB (5.52%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> hash;
+
+        for(int i = 0; i < n; i++){
+            hash[nums[i]] = i;
+        }
+
+        for(int i = 0; i < n; i++){
+            int comp = target - nums[i];
+            if(hash.find(comp) != hash.end() && hash[comp] != i)
+                return {i, hash[comp]};
+        }
+        return {};
+    }
+};
