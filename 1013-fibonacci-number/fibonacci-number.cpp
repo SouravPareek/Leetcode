bfbccf5eb4312ac304dc15e11bdf6ff85f02fe3f
Sourav Pareek <148560393+SouravPareek@users.noreply.github.com>
2026-03-03 21:18:09 +0530

    Time: 3 ms (65.34%) | Memory: 8.2 MB (15.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,19 +1,15 @@
-//self dp solution
+//self dp solution ysing tabulation
 class Solution {
-private:
-    int helper(vector<int>& dp, int n){
-        if(n <= 1)
-            return n;
-        
-        if(dp[n] != -1)
-            return dp[n];
-        
-        return dp[n] = helper(dp, n-1) + helper(dp, n-2);;
-    }
 public:
     int fib(int n) {
         vector<int> dp(n+1, -1);
+        dp[0] = 0;
+        if(n >= 1)
+            dp[1] = 1;
 
-        return helper(dp, n);
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
