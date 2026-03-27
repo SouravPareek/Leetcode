ef18bcb7dbec2bc0dbc6084834eab2c41b43e42d
Sourav Pareek <148560393+SouravPareek@users.noreply.github.com>
2026-03-27 17:38:54 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (91.30%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -1,31 +1,16 @@
+//2nd approach
 class Solution {
-private:
-    void leftRotate(vector<int>& arr, int k) {
-        reverse(arr.begin(), arr.begin() + k);
-        reverse(arr.begin() + k, arr.end());
-        reverse(arr.begin(), arr.end());
-    }
-    void rightRotate(vector<int>& arr, int k) {
-        reverse(arr.begin(), arr.end());
-        reverse(arr.begin(), arr.begin() + k);
-        reverse(arr.begin() + k, arr.end());
-    }
-
 public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
-        vector<vector<int>> original = mat;
         int n = mat.size();
         int m = mat[0].size();
 
-        k = k%m;
-
-        for (int j = 0; j < n; j++) {
-            if (j % 2 == 0) {
-                leftRotate(mat[j], k);
-            } else {
-                rightRotate(mat[j], k);
+        for (int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++){
+                if(mat[i][(j+k)%m] != mat[i][j])
+                    return false;
             }
         }
-        return mat == original;
+        return true;
     }
 };
