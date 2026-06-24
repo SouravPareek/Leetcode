5327f82b466c113f855305ba505e0e933a695cff
Sourav Pareek <148560393+SouravPareek@users.noreply.github.com>
2026-06-24 10:57:04 +0530

    Time: 7 ms (92.57%) | Memory: 13.4 MB (21.56%) - LeetSync

--- 2557-number-of-subarrays-with-lcm-equal-to-k/number-of-subarrays-with-lcm-equal-to-k.cpp
+++ 2557-number-of-subarrays-with-lcm-equal-to-k/number-of-subarrays-with-lcm-equal-to-k.cpp
@@ -1,3 +1,4 @@
+//bit optimized
 class Solution {
 public:
     int subarrayLCM(vector<int>& nums, int k) {
@@ -6,7 +7,11 @@ public:
         for(int i = 0; i < n; i++){
             int lcm_n = 1;
             for(int j = i; j < n; j++){
+                if(k%nums[j] != 0)
+                    break;
+                
                 lcm_n = lcm(lcm_n, nums[j]);
+
                 if(lcm_n == k)
                     cnt += 1;
                 else if(lcm_n > k)
