f236140e811796069020fb1a1f3039f039dd68ed
Sourav Pareek <148560393+SouravPareek@users.noreply.github.com>
2026-07-09 14:57:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (10.73%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,23 +1,16 @@
-//top down
+//bottom up
 class Solution {
-private:
-    int helper(int n, vector<int>& dp){
-        if(n <= 1)
-            return n;
-        
-        if(dp[n] != -1)
-            return dp[n];
-        
-        dp[n] = helper(n-1, dp) + helper(n-2, dp);
-
-        return dp[n];
-    }
 public:
     int fib(int n) {
+        if(n <= 1)
+            return n;
         vector<int> dp(n+1, -1);
 
-        dp[n] = helper(n, dp);
+        dp[0] = 0, dp[1] = 1;
 
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
     }
 };
