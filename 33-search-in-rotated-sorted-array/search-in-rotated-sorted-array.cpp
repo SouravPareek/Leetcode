0cc909c629c3103a327db9611f4645294618961c
Sourav Pareek <148560393+SouravPareek@users.noreply.github.com>
2026-07-14 11:50:55 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (34.85%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int left = 0, right = n-1;
+
+        while(left <= right){
+            int mid = (right-left)/2 + left;
+
+            if(nums[mid] == target)
+                return mid;
+            else if (nums[mid] >= nums[left]){
+                if(nums[mid] > target && target >= nums[left])
+                    right = mid-1;
+                else
+                    left = mid+1;
+            }else{
+                if(nums[mid] < target && target <= nums[right])
+                    left = mid+1;
+                else
+                    right = mid-1;
+            }
+        }
+        return -1;
+    }
+};
