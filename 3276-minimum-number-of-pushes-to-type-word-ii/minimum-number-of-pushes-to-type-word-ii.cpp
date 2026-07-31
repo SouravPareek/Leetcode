72dd3fc42e40996702cd369a39ed4db3c995c522
Sourav Pareek <148560393+SouravPareek@users.noreply.github.com>
2026-07-31 18:04:50 +0530

    Time: 10 ms (50.36%) | Memory: 26.3 MB (53.78%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,4 +1,4 @@
-//bit cleaner code(space optimized)
+//bit more cleaner code(space optimized)
 class Solution {
 public:
     int minimumPushes(string word) {
@@ -9,19 +9,18 @@ public:
             freq[ch-'a'] += 1;
         }
 
-        vector<pair<char,int>> arr;
+        vector<pair<int,int>> arr;
 
         for(int i = 0; i < 26; i++){
-            arr.push_back({i+'a', freq[i]});
+            if(freq[i] > 0)
+                arr.push_back({freq[i], i});
         }
 
-        sort(arr.begin(), arr.end(), [](const pair<char, int> a, const pair<char, int> b){
-            return a.second > b.second;
-        });
+        sort(arr.begin(), arr.end(), greater<>());
 
         vector<int> pos(26, 0);
         for(int i = 0; i < arr.size(); i++){
-            pos[arr[i].first-'a'] = (i/8) + 1;
+            pos[arr[i].second] = (i/8) + 1;
         }
 
         int ans = 0;
