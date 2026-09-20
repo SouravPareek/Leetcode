6b4ef1a5d217c2540ddf1a0a209643c5602258ad
Sourav Pareek <148560393+SouravPareek@users.noreply.github.com>
2026-09-20 08:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.88%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+
+        for(int i = 0; i < s.size(); i++){
+            int prod = (i+1) * ('z' - s[i] + 1);
+            ans += prod;
+        }
+
+        return ans;
+    }
+};
